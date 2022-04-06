f0f2af15daa63bd6e2fa8d0a3a80713c15ec8c91
ddguru <4668681@qq.com>
2022-04-06 10:46:39 +0800

    update readme

--- infrastructure/ecs_stack.py
+++ infrastructure/ecs_stack.py
@@ -273,7 +273,7 @@ class RosbagProcessor(core.Stack):
             stream_prefix="ecs",
             log_group=aws_logs.LogGroup(
                 self,
-                f"{image_name}-log-group",
+                f"{image_name}-ecs-log-group",
                 log_group_name=f"/ecs/{image_name}"
             ),
         )
